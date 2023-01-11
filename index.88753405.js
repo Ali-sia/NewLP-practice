b2438df6c915bcddf3e5838c68b3f9c9e7223261
Ali-sia <55650604+Ali-sia@users.noreply.github.com>
2023-01-11 14:08:07 +0000

    Deploying to gh-pages from @ Ali-sia/NewLP-practice@49eb938d380ae799a058c18a65b8487035337a90 🚀

--- index.88753405.js
+++
@@ -1,2 +0,0 @@
-const t=document.getElementsByClassName("faq_question-holder");for(let s=0;s<t.length;s++)t[s].addEventListener("click",e);function e(){this.classList.toggle("active");const t=this.nextElementSibling;t.classList.contains("toggle")?t.classList.remove("toggle"):t.classList.add("toggle")}
-//# sourceMappingURL=index.88753405.js.map
--- index.be912ba9.js
+++
@@ -1,2 +0,0 @@
-!function(){for(var t=document.getElementsByClassName("faq_question-holder"),e=0;e<t.length;e++)t[e].addEventListener("click",s);function s(){this.classList.toggle("active");var t=this.nextElementSibling;t.classList.contains("toggle")?t.classList.remove("toggle"):t.classList.add("toggle")}}();
-//# sourceMappingURL=index.be912ba9.js.map
