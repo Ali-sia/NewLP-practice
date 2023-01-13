9995b976ede165835f5efd609fde4cb8166740a9
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-01-13 19:59:14 +0200

    block scroll on page when slide menu active

--- src/js/slide-menu-active.js
+++ src/js/slide-menu-active.js
@@ -1,9 +1,12 @@
 const menuOpenBtn = document.querySelector('.header_open-slide-menu-btn');
 const menuCloseBtn = document.querySelector('.header_close-slide-menu-btn');
 const menu = document.querySelector('.header-slide-menu');
+const body = document.querySelector('body');
 menuOpenBtn.addEventListener('click', function () {
   menu.classList.add('active');
+  body.classList.add('block-scroll');
 });
 menuCloseBtn.addEventListener('click', function () {
   menu.classList.remove('active');
+  body.classList.remove('block-scroll');
 });
