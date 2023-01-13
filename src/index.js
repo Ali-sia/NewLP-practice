7c702bceb0ccbe6536f670dc22221315631d8535
aliesia-zahorui <55650604+Ali-sia@users.noreply.github.com>
2023-01-13 21:13:18 +0200

    hover focus links

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ShowHiddenBlock from './js/hidden-info-toggle';
 
 ShowHiddenBlock(document.getElementsByClassName('faq_question-holder'));
 ShowHiddenBlock(document.getElementsByClassName('details_title-holder'));
+ShowHiddenBlock(
+  document.getElementsByClassName('header-slide-menu_title-holder')
+);
 
 // налаштування слайдера
 import './js/slider';
